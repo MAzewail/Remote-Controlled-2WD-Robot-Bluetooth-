ab3b33b20b6f2df85e443a1973a33c0e2b8bcf7e
Mohamed Abdallah <110228201+MAzewail@users.noreply.github.com>
2023-08-10 12:34:28 +0300

    Add files via upload

---
+++ serial_2wd_task_v21.ino
@@ -0,0 +1,18 @@
+void setup()
+{
+  for (byte i=2;i<=5;i++)pinMode(i,1);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  char x=Serial.read();
+  for (byte i=0;i<4&&x!=-1;i++)digitalWrite(i+2,x&1<<i);
+}  
+
+
+// A --> Right
+// B --> Left
+// C --> Forward
+// L --> Backward
+// P --> Stop
